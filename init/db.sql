81c911105823e2c4ee57abe640ee8460f745b505
DimitrijeGlibic <dimitrije.glibic1@gmail.com>
2024-04-15 02:14:38 +0200

    Create db.sql

---
+++ init/db.sql
@@ -0,0 +1,4 @@
+CREATE SCHEMA IF NOT EXISTS user_service_schema AUTHORIZATION root;
+CREATE SCHEMA IF NOT EXISTS email_service_schema AUTHORIZATION root;
+CREATE SCHEMA IF NOT EXISTS bank_service_schema AUTHORIZATION root;
+CREATE SCHEMA IF NOT EXISTS exchange_service_schema AUTHORIZATION root;
